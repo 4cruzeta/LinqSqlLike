ea744608ebaf7861d0b4635d988d3228121e4e3b
4cruzeta <4cruzetam@gmail.com>
2019-11-22 20:29:39 -0300

    First commit

---
+++ LinqSqlLike/Entities/CategoryClass.cs
@@ -0,0 +1,9 @@
+﻿namespace LinqLambdaEx.Entities
+{
+    public class CategoryClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
---
+++ LinqSqlLike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqSqlLike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
